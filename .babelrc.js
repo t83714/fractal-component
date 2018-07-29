bf7b2fd949b10e34ecc085b940ce3fbaf5449e57
Jacky Jiang <t83714@gmail.com>
2018-07-29 20:42:36 +1000

    update babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,24 +1,25 @@
-const { NODE_ENV, BABEL_ENV } = process.env
+const { NODE_ENV, BABEL_ENV } = process.env;
 
-const cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test'
-const prod = NODE_ENV === 'production'
+const cjs = BABEL_ENV === "cjs" || NODE_ENV === "test";
+const prod = NODE_ENV === "production";
 
 module.exports = {
-  presets: [
-    [
-      '@babel/env',
-      {
-        loose: true,
-        modules: false,
-        exclude: ['transform-typeof-symbol'],
-        forceAllTransforms: true,
-      },
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                loose: true,
+                modules: false,
+                include: ["transform-typeof-symbol"],
+                useBuiltIns: "usage",
+                forceAllTransforms: true
+            }
+        ],
+        "@babel/preset-react",
+        "@babel/preset-stage-2"
     ],
-    '@babel/react',
-    '@babel/stage-2',
-  ],
-  plugins: [
-    cjs && '@babel/transform-modules-commonjs',
-    'annotate-pure-calls'
-  ].filter(Boolean),
-}
+    plugins: [
+        cjs && "@babel/transform-modules-commonjs",
+        "annotate-pure-calls"
+    ].filter(Boolean)
+};
