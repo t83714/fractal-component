c8e7eb0d1441e0df1ffca3560135d090d735504f
Jacky Jiang <t83714@gmail.com>
2018-08-15 22:56:53 +1000

    added utils definition

--- src/utils.d.ts
+++ src/utils.d.ts
@@ -4,4 +4,25 @@ const extraIs: {
     bool: GuardPredicate<boolean>;
 };
 
-export const is: (typeof extraIs & typeof reduxSagaIs);
+export const is: typeof extraIs & typeof reduxSagaIs;
+
+export const getPackageName: () => string;
+
+export const getPackageVersion: () => string;
+
+export const isDevMode: () => boolean;
+
+export const log: (message: string, level?: string, error?: any) => void;
+
+export const trim: (v: string) => void;
+
+export const konst: (v: any) => (() => any);
+export const kTrue: (v: any) => (() => boolean);
+export const kFalse: (v: any) => (() => boolean);
+export const noop: () => void;
+export const identity: (v: any) => any;
+
+export const is = {
+    ...reduxSagaIs,
+    bool: v => typeof v === "boolean"
+};
