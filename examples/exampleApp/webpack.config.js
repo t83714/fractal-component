95c75749c8ad56eb6295c89709bd7dca4a5c6d99
Jacky Jiang <t83714@gmail.com>
2018-09-05 22:27:31 +1000

    webpack config adjustment

--- examples/exampleApp/webpack.config.js
+++ examples/exampleApp/webpack.config.js
@@ -1,34 +1,53 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 
-module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, "src/main.js"),
-    resolve: {
-        alias: {
-            "fractal-component": path.resolve(__dirname, "../../packages/core/")
-        }
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new LodashModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "index.html")
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: ["babel-loader"]
+module.exports = (env, argv) => {
+    const devConfig = {
+        mode: "development",
+        entry: path.resolve(__dirname, "src/main.js"),
+        optimization: {
+            splitChunks: {
+                chunks: "all"
             }
-        ]
-    },
-    devtool: "source-map",
-    devServer: {
-        port: 3000,
-        hot: true
+        },
+        performance: {
+            hints: false
+        },
+        resolve: {
+            alias: {
+                "fractal-component": path.resolve(
+                    __dirname,
+                    "../../packages/core/"
+                )
+            }
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new LodashModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "index.html")
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: ["babel-loader"]
+                }
+            ]
+        },
+        devtool: "source-map",
+        devServer: {
+            port: 3000,
+            hot: true
+        }
+    };
+    if (argv.mode === "production") {
+        devConfig.mode = argv.mode;
+        delete devConfig.devServer;
+        devConfig.plugins.shift();
     }
+    return devConfig;
 };
