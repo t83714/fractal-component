b050f6e3c5f7515c6de396ac080594338d436de2
Jacky Jiang <t83714@gmail.com>
2018-10-01 10:42:00 +1000

    adjust effects apis creation logic -- avoid creating unnecessary func bind

--- packages/core/src/SagaRegistry/createEffects.js
+++ packages/core/src/SagaRegistry/createEffects.js
@@ -1,13 +1,16 @@
 import * as namespacedEffects from "./namespacedEffects";
 import * as rsEffects from "redux-saga/effects";
+import { is } from "../utils";
 
 const createEffects = (thisObj, SagaItem) => {
     const effectCreators = {};
-    Object.keys(rsEffects).forEach(idx => {
-        effectCreators[idx] = rsEffects[idx].bind(thisObj);
-    });
     Object.keys(namespacedEffects).forEach(idx => {
         effectCreators[idx] = namespacedEffects[idx].bind(thisObj, SagaItem);
+    });
+    Object.keys(rsEffects).forEach(idx => {
+        if (is.undef(effectCreators[idx])) {
+            effectCreators[idx] = rsEffects[idx].bind(thisObj);
+        }
     });
     return effectCreators;
 };
