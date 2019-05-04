c8262d785cdfc8694054238f0b83ba28f50efbe6
Jacky Jiang <t83714@gmail.com>
2019-05-04 19:30:47 +1000

    debug jest test from projetc root

--- jest.config.js
+++ jest.config.js
@@ -84,9 +84,7 @@ module.exports = {
     // ],
 
     // A map from regular expressions to module names that allow to stub out resources with a single module
-    moduleNameMapper: {
-        "fractal-component": "<rootDir>/packages/core/src/index.js"
-    },
+    // moduleNameMapper: {},
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
